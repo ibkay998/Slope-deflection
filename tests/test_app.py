e2b76dac3b9f3880a5673bbd391869908d969275
ibukunolouwa oyeniyi <ibukun46@gmail.com>
2019-12-03 09:35:39 +0100

    completed two span with testing

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,156 +1,54 @@
 # from sloper_deflection import SlopeDeflectionMethod
 import typing
-from working_draft import determine_moment, get_user_input1, LoadType, get_user_input, determine_moment1,determine_moment2
+from working_draft import LoadType, get_user_input,determine_moment2,EndCondition,get_user_input2,determine_moment3
 
 
-# def test_moment():
-#     # point scenario
-#     def assert_determine_moment(dataset, _result, decimal_place=None):
-#         result = determine_moment(dataset, decimal_places=decimal_place)
-#         assert result == _result
-
-#     assert_determine_moment(
-#         [
-#             {"span": 6, "magnitude": 10, "load_type": LoadType.POINT},
-#             {"span": 6, "magnitude": 8, "load_type": LoadType.POINT},
-#         ],
-#         (-7.875, 6.75, -6.75, 5.625),
-#     )
-#     # udl scenario
-#     assert_determine_moment(
-#         [
-#             {"span": 3, "magnitude": 10, "load_type": LoadType.UDL,},
-#             {"span": 4, "magnitude": 10, "load_type": LoadType.UDL,},
-#         ],
-#         (-1.69643, 2.23214, -2.23214, 2.63393,),
-#         decimal_place=5,
-#     )
-#     # vdl scenario
-#     assert_determine_moment(
-#         [
-#             {"span": 9, "magnitude": 30, "load_type": LoadType.VDL,},
-#             {"span": 4, "magnitude": 10, "load_type": LoadType.POINT,},
-#         ],
-#         (-1.69643, 2.23214, -2.23214, 2.63393,),
-#         decimal_place=5,
-#     )
-def test_moment2():
-    def assert_determine_moment2(dataset, _result, decimal_place=None):
+def test_moment_endCondition():
+    def assert_determine_moment2(dataset,_result, decimal_place=None):
         result = determine_moment2(dataset,decimal_places=decimal_place)
         assert result == _result
 
     assert_determine_moment2(
         [
-            {"span": 4, "magnitude": 10, "load_type": LoadType.POINT, "distance":0,"order":0},
-            {"span": 6, "magnitude": 20, "load_type": LoadType.VDL,"distance":0,"order":1},
+            {"span": 4, "magnitude": 10, "load_type": LoadType.POINT,"end_conditions":EndCondition.FIXED, "distance":0,"order":0},
+            {"span": 6, "magnitude": 20, "load_type": LoadType.VDL,"end_conditions":EndCondition.ROLLER,"distance":0,"order":1},
         ],
         (7.33,29.67,-29.67, 0),
         decimal_place=2,
     )
 
-
-def test_moment1():
-    def assert_determine_moment1(dataset, _result, decimal_place=None):
-        result = determine_moment1(dataset,decimal_places=decimal_place)
+def test_moment():
+    def assert_determine_moment2(datasetA, datasetB,_result, decimal_place=None):
+        result = determine_moment3(datasetA,datasetB,decimal_places=decimal_place)
         assert result == _result
 
-    
-    
-    assert_determine_moment1(
-        [
-            {"span": 6, "magnitude": 20, "load_type": LoadType.UDL, "distance":0,"order":0},
-            {"span": 8, "magnitude": 20, "load_type": LoadType.POINT,"distance":0,"order":0},
-        ],
-        (-71.43, 37.14, -37.14, 11.43,),
+    assert_determine_moment2(
+                [
+                    {"span": 6, "magnitude": 10, "load_type": LoadType.POINT, "end_conditions":EndCondition.FIXED, "distance":2,"order":0},
+                    {"span": 6, "magnitude": 8, "load_type": LoadType.UDL, "end_conditions":EndCondition.FIXED, "distance":2,"order":0,},
+                ],
+                [
+                    {"span": 3, "magnitude": 20, "load_type": LoadType.POINT,"end_conditions":EndCondition.FIXED, "distance":2,"order":0},
+                ],
+        (7.33,29.67,-29.67, 0),
         decimal_place=2,
     )
 
-    assert_determine_moment1(
+def test_fem():
+    dataset=[
+                [
+                    {"span": 3, "magnitude": 10, "load_type": LoadType.POINT, "end_conditions":EndCondition.FIXED, "distance":2,"order":0},
+                    
+                ],
+            ]
+    datasetB=[
         [
-            {"span": 6, "magnitude": 5, "load_type": LoadType.NOPOINT, "distance":2,"order":0},
-            {"span": 6, "magnitude": 20, "load_type": LoadType.VDL,"distance":0,"order":2},
-        ],
-        (4.00, 19.11, -19.11, 32.44,),
-        decimal_place=2,
-    )
-
-    assert_determine_moment1(
-        [
-            {"span": 10, "magnitude": 30, "load_type": LoadType.UDL, "distance":0,"order":0},
-            {"span": 10, "magnitude": 20, "load_type": LoadType.NOPOINT,"distance":4,"order":0},
-        ],
-        (-305.3, 139.4, -139.4, -36.1,),
-        decimal_place=2,
-    )
-    
+            {"span": 4, "magnitude": 20, "load_type": LoadType.POINT,"end_conditions":EndCondition.FIXED, "distance":2,"order":0},
+        ]
+    ]
 
 
-# def test_FEM_for_point():
-#     dataset = [
-#         [
-#             [
-#                 {"span": 6, "magnitude": 10, "load_type": LoadType.POINT},
-#                 {"span": 6, "magnitude": 10, "load_type": LoadType.UDL},
-#             ],
-#             [30, 30, 6, 6],
-#         ],
-        
-#     ]
-#     released = dataset[0][0]
-#     value = [x.get("load_type",None) for x in released]
-#     for i in dataset:
-#         for j in value:
-#             assert get_user_input1(i[0],j) == i[1]
+    for i in dataset:
+        for j in datasetB:
+            assert get_user_input2(i,j) ==  [9.75, 7.5, 3, 4]
 
-
-def test_FEM_for_any_loadtype():
-    def assert_determine_fem(dataset, _result, decimal_place=None):
-        result = get_user_input(dataset,decimal_places=decimal_place)
-        assert result == _result
-    assert_determine_fem(
-        [
-            {"span": 6, "magnitude": 5, "load_type": LoadType.NOPOINT, "distance":2,"order":0},
-            {"span": 6, "magnitude": 20, "load_type": LoadType.VDL,"distance":0,"order":2},
-        ],
-        (4.44,36,2.22,24, 6, 6),
-        decimal_place=2,
-    )
-    assert_determine_fem(
-        [
-           {"span": 10, "magnitude": 30, "load_type": LoadType.UDL, "distance":0,"order":0},
-           {"span": 10, "magnitude": 20, "load_type": LoadType.NOPOINT,"distance":4,"order":0},
-        ],
-        (250,28.8,250,19.2, 10, 10),
-        decimal_place=2,
-    )
-    assert_determine_fem(
-        [
-           {"span": 4, "magnitude": 10, "load_type": LoadType.POINT, "distance":0,"order":0},
-           {"span": 6, "magnitude": 20, "load_type": LoadType.VDL,"distance":0,"order":1},
-        ],
-        (5,24,5,36, 4,6),
-        decimal_place=2,
-    )
-
-# def test_other_types_of_load():
-#     # point test
-#     dataset = [
-#         {"span": 6, "magnitude": 10, "load_type": LoadType.POINT},
-#         {"span": 6, "magnitude": 8, "load_type": LoadType.POINT},
-#     ]
-#     result = get_user_input(dataset)
-#     assert result == [7.5, 6.0, 6, 6]
-#     # udl test
-#     dataset = [
-#         {"span": 3, "magnitude": 10, "load_type": LoadType.UDL,},
-#         {"span": 4, "magnitude": 10, "load_type": LoadType.UDL,},
-#     ]
-#     result = get_user_input(dataset)
-#     assert result == [1.875, 2.5, 3, 4]
-#     # vdl test
-#     dataset = [
-#         {"span": 9, "magnitude": 30, "load_type": LoadType.VDL,},
-#         {"span": 4, "magnitude": 10, "load_type": LoadType.UDL,},
-#     ]
-#     result = get_user_input(dataset)
-#     assert result == [1.875, 2.5, 3, 4]
--- tests/working_draft.py
+++ tests/working_draft.py
@@ -9,72 +9,12 @@ class LoadType(enum.Enum):
     NOPOINT="nopoint"
     UDL = "udl"
     VDL = "vdl"
+    MOMENT="moment"
     COMB= "comb"
 
-
-def determine_moment(dataset: typing.List[typing.Any], decimal_places: int = None, Value=None):
-    result = get_user_input1(dataset,Value)
-    a, b, c, d = result
-    i1 = 1
-    i2 = 1
-    e = i1 * d
-    f = i2 * c
-    oA = 0
-    oC = 0
-    deflection = 0
-    s = 0 - ((a) - (b))
-    E = 1
-    oB = (
-        (s * c * d) / (2 * E)
-        - (e * oA)
-        + (e * 3 * deflection)
-        - (f * oC)
-        - (f * 3 * deflection)
-    ) / (2 * (e + f))
-    MAB = ((2 * E * i1) / c) * ((2 * oA) + oB - 3 * deflection) - a
-    MBA = ((2 * E * i1) / c) * ((2 * oB) + oA - 3 * deflection) + a
-    MBC = ((2 * E * i2) / d) * ((2 * oB) + oC - 3 * deflection) - b
-    MCB = ((2 * E * i2) / d) * ((2 * oC) + oB - 3 * deflection) + b
-    print(MAB)
-    print(MBA)
-    print(MBC)
-    print(MCB)
-    to_decimal_places = lambda x: float("%.{}f".format(decimal_places) % x)
-    if decimal_places:
-        return tuple([to_decimal_places(x) for x in [MAB, MBA, MBC, MCB]])
-    return MAB, MBA, MBC, MCB
-
-def determine_moment1(dataset: typing.List[typing.Any], decimal_places: int = None):
-    result = get_user_input(dataset)
-    a, c, b, d, x, y = result
-    i1 = 1
-    i2 = 1
-    e = i1 * x
-    f = i2 * y
-    oA = 0
-    oC = 0
-    deflection = 0
-    s = 0 - ((b) - (c))
-    E = 1
-    oB = (
-        (s * x * y) / (2 * E)
-        - (e * oA)
-        + (e * 3 * deflection)
-        - (f * oC)
-        - (f * 3 * deflection)
-    ) / (2 * (e + f))
-    MAB = ((2 * E * i1) / x) * ((2 * oA) + oB - 3 * deflection) - a
-    MBA = ((2 * E * i1) / x) * ((2 * oB) + oA - 3 * deflection) + b
-    MBC = ((2 * E * i2) / y) * ((2 * oB) + oC - 3 * deflection) - c
-    MCB = ((2 * E * i2) / y) * ((2 * oC) + oB - 3 * deflection) + d
-    print(MAB)
-    print(MBA)
-    print(MBC)
-    print(MCB)
-    to_decimal_places = lambda x: float("%.{}f".format(decimal_places) % x)
-    if decimal_places:
-        return tuple([to_decimal_places(x) for x in [MAB, MBA, MBC, MCB]])
-    return MAB, MBA, MBC, MCB
+class EndCondition(enum.Enum):
+    FIXED = "fixed"
+    ROLLER = "roller"
 
 def determine_moment2(dataset: typing.List[typing.Any], decimal_places: int = None):
     result = get_user_input(dataset)
@@ -111,6 +51,38 @@ def determine_moment2(dataset: typing.List[typing.Any], decimal_places: int = No
         return tuple([to_decimal_places(x) for x in [MAB, MBA, MBC, MCB]])
     return MAB, MBA, MBC, MCB
 
+def determine_moment3(datasetA: typing.List[typing.Any],datasetB, decimal_places: int = None):
+    result = get_user_input2(datasetA,datasetB)
+    a, b, c, d, x, y = result
+    i1 = 1
+    i2 = 1
+    e = i1 * x
+    f = i2 * y
+    oA = 0
+    oC = 0
+    deflection = 0
+    s = 0 - ((b) - (c))
+    E = 1
+    oB = (
+        (s * x * y) / (2 * E)
+        - (e * oA)
+        + (e * 3 * deflection)
+        - (f * oC)
+        - (f * 3 * deflection)
+    ) / (2 * (e + f))
+    MAB = ((2 * E * i1) / x) * ((2 * oA) + oB - 3 * deflection) - a
+    MBA = ((2 * E * i1) / x) * ((2 * oB) + oA - 3 * deflection) + b
+    MBC = ((2 * E * i2) / y) * ((2 * oB) + oC - 3 * deflection) - c
+    MCB = ((2 * E * i2) / y) * ((2 * oC) + oB - 3 * deflection) + d
+    print(MAB)
+    print(MBA)
+    print(MBC)
+    print(MCB)
+    to_decimal_places = lambda x: float("%.{}f".format(decimal_places) % x)
+    if decimal_places:
+        return tuple([to_decimal_places(x) for x in [MAB, MBA, MBC, MCB]])
+    return MAB, MBA, MBC, MCB
+
 def get_user_input(data_input: typing.List[typing.Any],decimal_places: int = None):
     result = [get_Complete(**x) for x in data_input]
     first_tuple_items = [x[0] for x in result]
@@ -121,77 +93,94 @@ def get_user_input(data_input: typing.List[typing.Any],decimal_places: int = Non
         return tuple([to_decimal_places(x) for x in [*first_tuple_items,*middle_tuple_items,*last_tuple_items]])
     return [*first_tuple_items,*middle_tuple_items,*last_tuple_items]
 
-def get_user_input1(data_input,value=None):
-    result = None
-    if value == LoadType.UDL:
-        result = [get_FEM(**x) for x in data_input]
-    elif value==LoadType.POINT:
-        result = [get_FEM(**x) for x in data_input]
-    elif value==LoadType.VDL:
-        result = [get_FEM_VDL(**x) for x in data_input]
-    elif value == LoadType.NOPOINT:
-        result = [get_FEM_NOPOINT_LOAD(**x) for x in data_input]
-    else: 
-        result= []
-    first_tuple_items = [x[0] for x in result]
-    last_tuple_items = [x[1] for x in result]
-    return [*first_tuple_items, *last_tuple_items]
+def get_user_input2(data_input: typing.List[typing.Any],data_input2):
+    result1 = [get_Complete(**x) for x in data_input]
+    result2 = [get_Complete(**x) for x in data_input2]
+    first=[]
+    second=[]
+    third=[]
+    fourth=[]
+    span1 = [i[2] for i in result1]
+    span2 = [i[2] for i in result2]
+    for i in result1:
+        first.append(i[0])
+        second.append(i[1])
+    for j in result2:
+        third.append(j[0])
+        fourth.append(j[1])
+   
+    
+    return [sum(first),sum(second),sum(third),sum(fourth),span1[0],span2[0]]
 
 
-def get_FEM(load_type: LoadType = None, span=None, magnitude=None):
-    FEM_point=None
-    if load_type == LoadType.POINT:
-        FEM_point = (span * magnitude) / 8
-    elif load_type == LoadType.UDL:
-        FEM_point = (span * span * magnitude) / 12
-    else:
-        FEM_point=0
-    return FEM_point,span
+def get_Complete(load_type: LoadType = None, span=None, end_conditions: EndCondition = None, magnitude=None,distance=None,order=None):
+    if end_conditions==EndCondition.FIXED: 
+        if load_type == LoadType.POINT:
+            FEM_point = (span * magnitude) / 8
+            return FEM_point,FEM_point,span
+        elif load_type == LoadType.UDL:
+            FEM_pointu = (span * span * magnitude) / 12
+            return FEM_pointu,FEM_pointu,span
 
+        elif load_type == LoadType.MOMENT:
+            a = distance
+            b = span - distance
+            FEM_pointm = (b * magnitude)*((2*a)-b) /(span * span)
+            FEM_pointu = (a * magnitude)*((2*b)-a) /(span * span)
+            return FEM_pointm,FEM_pointu,span
 
-def get_FEM_NOPOINT_LOAD(load_type: LoadType = None, span=None, magnitude=None,distance=None,position_of_span=None):
-    a = distance
-    b = span - distance
-    FEM_not_center_A = 0 - ((magnitude * a * b * b) / span * span)
-    FEM_not_center_B = ((magnitude * a * a * b) / span * span)
-
-    return FEM_not_center_A, FEM_not_center_B
-
-def get_FEM_VDL(load_type: LoadType = None, span=None, magnitude=None,position_of_span=None,order=None):
-    FEM_point1 = (span * span * magnitude)/30
-    FEM_point2 = (span * span * magnitude)/20
-    if position_of_span == 1 and order== 1:
-        return FEM_point2,span
-    elif position_of_span ==2 and order== 1:
-        return FEM_point1,span
-    elif position_of_span==1 and order ==2:
-        return FEM_point1,span
-    elif position_of_span==2 and order ==2:
-        return FEM_point2,span
-    else:
-        return 0,span
-
-def get_Complete(load_type: LoadType = None, span=None, magnitude=None,distance=None,order=None):
-    if load_type == LoadType.POINT:
-        FEM_point = (span * magnitude) / 8
-        return FEM_point,FEM_point,span
-    elif load_type == LoadType.UDL:
-        FEM_pointu = (span * span * magnitude) / 12
-        return FEM_pointu,FEM_pointu,span
-    elif load_type == LoadType.VDL:
-        FEM_point1 = (span * span * magnitude)/30
-        FEM_point2 = (span * span * magnitude)/20
-        if order== 1:
-            return FEM_point1,FEM_point2,span
-        elif order ==2:
-            return FEM_point2,FEM_point1,span
+        elif load_type == LoadType.VDL:
+            FEM_point1 = (span * span * magnitude)/30
+            FEM_point2 = (span * span * magnitude)/20
+            if order== 1:
+                return FEM_point1,FEM_point2,span
+            elif order ==2:
+                return FEM_point2,FEM_point1,span
+            else:
+                return 0,0,span
+        elif load_type == LoadType.NOPOINT:
+            a = distance
+            b = span - distance
+            FEM_not_center_A = ((magnitude * a * b * b) / (span * span))
+            FEM_not_center_B = ((magnitude * a * a * b) / (span * span))    
+            return FEM_not_center_A,FEM_not_center_B,span
         else:
-            return 0,0,span
-    elif load_type == LoadType.NOPOINT:
-        a = distance
-        b = span - distance
-        FEM_not_center_A = ((magnitude * a * b * b) / (span * span))
-        FEM_not_center_B = ((magnitude * a * a * b) / (span * span))    
-        return FEM_not_center_A,FEM_not_center_B,span
-    else:
+            pass
+
+    elif end_conditions==EndCondition.ROLLER:
+        if load_type == LoadType.POINT:
+            FEM_point = (3*magnitude * span)/16
+            return FEM_point,0,span
+
+        elif load_type == LoadType.NOPOINT:
+            a = distance
+            b = span - distance
+            FEM_not_center= ((magnitude  / (span * span)*((b*b*a) +((a*a*b)/2))))
+            return FEM_not_center,0,span
+
+        elif load_type == LoadType.MOMENT:
+            a = distance
+            b = span - distance
+            FEM_pointm = (b * magnitude)*((2*a)-b) /(span * span)
+            FEM_pointu = (a * magnitude)*((2*b)-a) /(span * span)
+            return FEM_pointm,0,span
+
+        elif load_type == LoadType.UDL:
+            FEM_pointu = (span * span * magnitude) / 8
+            return FEM_pointu,0,span
+        
+        elif load_type == LoadType.VDL:
+            FEM_pointv = (span * span * magnitude) / 15
+            return FEM_pointv,0,span
+        
+        else:
+            pass
+    else: 
         pass
+
+
+
+
+
+
+    
